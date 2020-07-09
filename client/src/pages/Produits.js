d0afca6aa2c417384a23378e39de343a24e0ce6d
khaoula <tti.khaoula@gmail.com>
2020-07-09 21:30:45 +0100

    search product

--- client/src/pages/Produits.js
+++ client/src/pages/Produits.js
@@ -1,24 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector,useDispatch } from 'react-redux'
 
 import { getAllProduits } from '../actions'
-import { ProduitCard } from '../components'
+import { ProduitCard, Search } from '../components'
 
 
 
 function Produits() {
+    const [inputOpen, setInputOpen] = useState("");
+    const [inputValue, setInputValue] = useState("");
+    
     const produits = useSelector(state => state.produits)
-
     const dispatch = useDispatch();
+
     useEffect(() => {
      dispatch(getAllProduits());
     }, [])
 
+    const onSearch = (toSearch) => {
+        switch(inputOpen) {
+            case "Nom":
+                return toSearch.filter(el => (
+                    el.nom.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1
+                )) 
+
+            case "Type":
+                return toSearch.filter(el => (
+                    el.type.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1
+               ))
+            
+            default:
+                return toSearch
+        }
+    }
+
     return (
-        <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}} >
-            {produits.produits.map(el => (
-                <ProduitCard el={el} key={el._id} />   
-            ))}
+        <div>
+            <Search 
+                    setInputOpen={setInputOpen} 
+                    inputOpen={inputOpen} 
+                    setInputValue={setInputValue} 
+                />
+
+            <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}} >
+                {onSearch(produits.produits).map(el => (
+                    <ProduitCard el={el} key={el._id} />   
+                ))}
+            </div>
         </div>
     )
 }
